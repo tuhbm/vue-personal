c4a9ea85ae2d24262c6a39fc9946d90ad1152582
taekyunKimDev <powerging2@nate.com>
2017-09-15 14:55:15 +0900

    vue 공부 시작

---
+++ 170915/app.js
@@ -0,0 +1,57 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    name: '태균',
+    job: '프론트엔드개발자',
+    website: 'https://tuhbm.github.io',
+    websiteTag:'<a href="https://tuhbm.github.io">블로그바로가기</a>'
+  },
+  methods: {
+    greet: function (time) {
+      return '안녕하세요 좋은 ' + time + '입니다.';
+      // return '안녕하세요';
+    }
+  }
+});
+
+new Vue({
+  el: '#vue-app2',
+  data: {
+    age:29,
+    x:0,
+    y:0
+  },
+  methods: {
+    add:function(inc){
+      this.age+=inc;
+    },
+    minus:function(dec){
+      this.age-=dec;
+    },
+    updataXY:function(event){
+      // console.log(event);
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+
+    },
+    click: function(){
+      alert('You clicked me');
+    }
+  }
+});
+new Vue({
+  el:'#vue-app3',
+  data:{
+
+  },
+  methods:{
+    logName:function(){
+      console.log('You entered your name');
+    },
+    logAge:function(){
+      console.log('You entered your age');
+    }
+  }
+});
+
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# vue 공부
+
+## 공부목록
+- text binding
+- function binding
+- v-bind
+- v-html
+- v-on
+    * click
+        + .prevent
+    * mouseover
+    * key
+        + .enter
+        + .alt
